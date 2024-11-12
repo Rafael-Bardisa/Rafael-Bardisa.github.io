7667924526a475e23211558faa0d2542eec7c48f
Rafael-Bardisa <72401330+Rafael-Bardisa@users.noreply.github.com>
2024-11-12 21:53:03 +0100

    test directory listing

---
+++ index.js
@@ -0,0 +1,13 @@
+(async () => {
+    const response = await fetch('https://api.github.com/repos/Rafael-Bardisa/Rafael-Bardisa.github.io/contents/');
+    const data = await response.json();
+    let htmlString = '<ul>';
+    
+    for (let file of data) {
+      htmlString += `<li><a href="${file.path}">${file.name}</a></li>`;
+    }
+
+    htmlString += '</ul>';
+    document.getElementsById('test')[0].innerHTML = htmlString;
+    console.log(htmlString);
+  })()
